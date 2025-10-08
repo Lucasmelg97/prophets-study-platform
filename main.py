353d4d02a862197b25db9b51284a00f182315e2e
Lucas Melgaço <34048068+Lucasmelg97@users.noreply.github.com>
2025-10-08 12:01:31 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# Substitua pelo seu link do GitHub Pages
+url = "https://lucasmelg97.github.io/prophets-study-platform/plataforma_estudos_DS.html"
+
+print("Abrindo a plataforma...")
+webbrowser.open(url)
